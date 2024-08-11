c3865e645b05daaa0d775dd48503687922c7958d
Francisco Leal <francisco@talentprotocol.com>
2024-08-11 02:39:30 +0100

    feat: add base UI

--- src/app/attestations/page.tsx
+++ src/app/attestations/page.tsx
@@ -1,7 +1,10 @@
 import { getCurrentUser } from "@/server/users";
 import { UserNotConnected } from "@/components/user-not-connected";
-import { getCredentialsForPassport } from "@/server/talent-protocol";
-import { Typography, List, ListItem, Button } from "@mui/joy";
+import {
+  getCredentialsForPassport,
+  getTalentPassport,
+} from "@/server/talent-protocol";
+import { Typography, Button, Card, Grid, Box, Avatar } from "@mui/joy";
 
 export default async function AttestationsPage() {
   const user = await getCurrentUser();
@@ -12,24 +15,61 @@ export default async function AttestationsPage() {
       <h1>Create your passport and easily create your first attestation</h1>
     );
   }
+
+  const passport = await getTalentPassport(user.wallet);
   const passportCredentials = await getCredentialsForPassport(user.passportId);
 
   return (
     <>
-      <Typography level="h3">Attestations</Typography>
-      <Typography level="body-md">
-        Based on your Talent Passport we&apos;ve found the following credentials
-      </Typography>
-      <List variant="outlined" marker="disc" sx={{ paddingRight: 2 }}>
-        {passportCredentials.map((credential) => (
-          <ListItem key={credential.id}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          marginX: "auto",
+          paddingX: { sm: 6, md: 8, lg: 12 },
+          paddingY: 12,
+          gap: 4,
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Avatar
+            sx={{ width: 80, height: 80, marginRight: 4 }}
+            src={user.profile.image_url}
+          >
+            {user.profile.name}
+          </Avatar>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              marginRight: "auto",
+            }}
+          >
+            <Typography level="body-lg">{user.profile.name}</Typography>
             <Typography level="body-md">
-              {credential.name}: {credential.score}
+              {passport?.passport_profile?.bio}
             </Typography>
-          </ListItem>
-        ))}
-      </List>
-      <Button>Generate Attestation</Button>
+            <Typography level="body-lg">
+              Score: {passport?.score || 0}
+            </Typography>
+          </Box>
+          <w3m-button balance={"hide"} />
+        </Box>
+        <Typography level="h3">Your Talent Passport credentials</Typography>
+        <Grid container spacing={2} sx={{ flexGrow: 1, width: "100%" }}>
+          {passportCredentials.map((credential) => (
+            <Grid key={credential.id} xs={6} md={3} lg={3}>
+              <Card>
+                <Typography level="title-lg">{credential.name}</Typography>
+                <Typography level="body-sm">
+                  Score: {credential.score}
+                </Typography>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+        <Button>Generate Attestation</Button>
+      </Box>
     </>
   );
 }
